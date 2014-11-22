beb1634e727187d6c02bcef34e9a7e4cf80e6ffc
Brent Palmer <bmpalmer987@gmail.com>
2014-11-22 01:47:03 -0800

    Player turn almost complete for Web App Blackjack.

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -4,6 +4,38 @@ require 'pry'
 
 set :sessions, true
 
+helpers do
+
+  def calculate_total(cards)
+     arr = cards.map {|e| e[1]}
+
+    total = 0
+    arr.each do |value|
+      if value == "A"
+        total += 11
+      elsif value.to_i == 0
+        total += 10
+      else
+        total += value.to_i
+      end
+    end
+
+  arr.select{|e| e == "A"}.count.times do
+    if total > 21
+      total -= 10
+    end
+  end
+  total
+  end
+
+end
+
+
+before do
+@show_hit_or_stay_buttons = true
+end
+
+
 get '/' do
   if session[:username]
     redirect '/game'
@@ -11,6 +43,7 @@ get '/' do
     redirect '/set_name'
   end
 end
+
 
 get '/set_name' do
   erb :set_name
@@ -21,8 +54,8 @@ post '/set_name' do
   redirect '/game'
 end
 
-get '/game' do
 
+get '/game' do
   suits = ["H", "C", "D", "S"]
   values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
   session[:deck] = suits.product(values).shuffle!
@@ -35,8 +68,26 @@ get '/game' do
     session[:player_cards] << session[:deck].pop
   end
 
+  erb :game
+end
 
 
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  if calculate_total(session[:player_cards]) > 21
+    @error = "#{session[:username]} busts with: #{calculate_total(session[:player_cards])}. Dealer wins!"
+    @show_hit_or_stay_buttons = false
+  end
+
+  erb :game
+end
+
+
+post '/game/player/stay' do
+  @success = "You have chosen to stay."
+  @show_hit_or_stay_buttons = false
+  
   erb :game
 end
 
