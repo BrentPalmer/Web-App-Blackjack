083ff8f576df64a761b6e9c75aa69fdcbe330d6a
Brent Palmer <bmpalmer987@gmail.com>
2014-11-22 00:51:21 -0800

    initial game set up for Web App Blackjack.

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -4,4 +4,39 @@ require 'pry'
 
 set :sessions, true
 
+get '/' do
+  if session[:username]
+    redirect '/game'
+  else
+    redirect '/set_name'
+  end
+end
+
+get '/set_name' do
+  erb :set_name
+end  
+
+post '/set_name' do
+  session[:username] = params[:username]
+  redirect '/game'
+end
+
+get '/game' do
+
+  suits = ["H", "C", "D", "S"]
+  values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+
+  2.times do
+    session[:dealer_cards] << session[:deck].pop
+    session[:player_cards] << session[:deck].pop
+  end
+
+
+
+  erb :game
+end
 
