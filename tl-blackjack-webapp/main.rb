992bc15a35210477d4e22bbf125ce63fd468156e
Brent Palmer <bmpalmer987@gmail.com>
2014-11-26 09:23:11 -0800

    Web App Blackjack game

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -8,6 +8,10 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'random'
 
+BLACKJACK_AMOUNT = 21
+MIN_DEALER_HIT = 17
+STARTING_MONEY = 500
+
 helpers do
 
   def calculate_total(cards)
@@ -25,7 +29,7 @@ helpers do
     end
 
   arr.select{|e| e == "A"}.count.times do
-    if total > 21
+    if total > BLACKJACK_AMOUNT
       total -= 10
     end
   end
@@ -55,14 +59,34 @@ helpers do
 
   def win_or_lose(player_total, dealer_total)
     if player_total > dealer_total
-      @success = "Congradulations, #{session[:username]} wins!"
+      winner("Congradulations!")
     elsif player_total < dealer_total
-      @error = "#{session[:username]} lost! Dealer has: #{dealer_total}, dealer wins!" 
+      loser("Dealer wins!")
     else
-      @success = "It's a tie!"
+      tie("No money lost!")
     end
   end
 
+  def winner(msg)
+    @success = "#{session[:username]} wins!"
+    @show_hit_or_stay_buttons = false
+    @play_again = true
+    session[:player_total_money] = session[:player_total_money] + session[:bet_amount]
+  end
+
+  def loser(msg)
+    @error = "#{session[:username]} loses!"
+    @show_hit_or_stay_buttons = false
+    @play_again = true
+    session[:player_total_money] = session[:player_total_money] - session[:bet_amount]
+  end
+
+  def tie(msg)
+    @success = "Its a tie!"
+    @show_hit_or_stay_buttons = false
+    @play_again = true
+    session[:player_total_money] = session[:player_total_money]
+  end
 
 end
 
@@ -70,12 +94,13 @@ end
 before do
   @show_hit_or_stay_buttons = true
   @hide_card = true
+  @play_again = false
 end
 
 
 get '/' do
   if session[:username]
-    redirect '/game'
+    redirect '/bet'
   else
     redirect '/set_name'
   end
@@ -100,6 +125,26 @@ post '/set_name' do
     
   end
   session[:username] = params[:username]
+  session[:player_total_money] = STARTING_MONEY
+  redirect '/bet'
+end
+
+
+get '/bet' do
+  if session[:player_total_money] == 0
+    redirect '/game_over'
+  end
+  erb :bet
+end
+
+
+post '/bet' do
+  session[:player_total_money] = session[:player_total_money]
+  session[:bet_amount] = params[:bet_amount].to_i
+  if session[:bet_amount] < 1 ||  session[:bet_amount] > session[:player_total_money]
+    @error = "Bet amount must be between $1 and $#{session[:player_total_money]}"
+    halt erb(:bet)
+  end
   redirect '/game'
 end
 
@@ -126,12 +171,10 @@ post '/game/player/hit' do
 
   player_total = calculate_total(session[:player_cards])
 
-  if player_total == 21
-    @success = "Congradulations, #{session[:username]} hit blackjack!"
-    @show_hit_or_stay_buttons = false
-  elsif player_total > 21
-    @error = "#{session[:username]} busts with: #{calculate_total(session[:player_cards])}. Dealer wins!"
-    @show_hit_or_stay_buttons = false
+  if player_total == BLACKJACK_AMOUNT
+    winner("#{session[:username]} hit blackjack!")
+  elsif player_total > BLACKJACK_AMOUNT
+    loser("#{session[:username]} busts with: #{calculate_total(session[:player_cards])}. Dealer wins!")
     @hide_card = false
   end
 
@@ -152,11 +195,11 @@ get '/game/dealer' do
   @hide_card = false
 
   dealer_total = calculate_total(session[:dealer_cards])
-  if dealer_total == 21
-    @error = "Sorry, Dealer hit Blackjack!"
-  elsif dealer_total > 21
-    @success = "Dealer busted! #{session[:username]} wins!"
-  elsif dealer_total >= 17
+  if dealer_total == BLACKJACK_AMOUNT
+    loser("Sorry, Dealer hit Blackjack!")
+  elsif dealer_total > BLACKJACK_AMOUNT
+    winner("Dealer busted!")
+  elsif dealer_total >= MIN_DEALER_HIT
     redirect '/game/compare'
   else
     @show_dealer_hit_button = true
@@ -172,9 +215,9 @@ post '/game/dealer/hit' do
   session[:dealer_cards] << session[:deck].pop
 
   dealer_total = calculate_total(session[:dealer_cards])
-  if dealer_total > 21
-    @success = "Dealer busted! #{session[:username]} wins!"
-  elsif dealer_total >= 17
+  if dealer_total > BLACKJACK_AMOUNT
+    winner("Dealer busted!")
+  elsif dealer_total >= MIN_DEALER_HIT
     redirect '/game/compare'
   else
     @show_dealer_hit_button = true
@@ -192,6 +235,11 @@ get '/game/compare' do
 
   erb :game
 end
+
+get '/game_over' do
+  erb :game_over
+end
+
     
 
 
